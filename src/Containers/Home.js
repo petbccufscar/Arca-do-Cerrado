0230d4d74ef69c64cc5a260531b1152680491141
Thaís Dordan <thais.dordan@liven.tech>
2020-11-29 11:36:55 -0300

    adding carousel

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,10 +1,11 @@
 import React, { Component, Fragment } from 'react'
+import Carousel from '../Components/Carousel/Carousel'
 
 class Home extends Component {
   render () {
     return (
       <Fragment>
-        <text>Tela inicial</text>
+        <Carousel />
       </Fragment>
     )
   }
---
+++ src/Themes/Images.js
@@ -0,0 +1,7 @@
+const images = {
+  first: require('../Images/CarouselHome/1.jpeg'),
+  second: require('../Images/CarouselHome/2.jpeg'),
+  third: require('../Images/CarouselHome/3.jpeg')
+}
+
+export default images
